e3ad0825b2e985998041446b17dcbf2562da20f0
toann7700@gmail.com <toann7700@gmail.com>
2021-10-23 21:42:30 +0700

    API get all user

--- src/main/java/com/comaymanagement/cmd/controller/APIController.java
+++ src/main/java/com/comaymanagement/cmd/controller/APIController.java
@@ -1,10 +1,31 @@
 package com.comaymanagement.cmd.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.comaymanagement.cmd.entity.User;
+import com.comaymanagement.cmd.service.UserService;
+
+@RestController
+@RequestMapping("/")
 public class APIController {
 	
-
-
-
-
+	@Autowired
+	UserService userService;
+	
+	
+	
+	// get all user
+	@GetMapping(path = "/users", produces = "application/json")
+	public List<User> findAll(){
+		
+		return userService.findAll();
+		
+	}
+	
 	
 }
---
+++ src/main/java/com/comaymanagement/cmd/entity/User.java
@@ -0,0 +1,15 @@
+package com.comaymanagement.cmd.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	String name;
+	int tuoi;
+	
+	
+}
