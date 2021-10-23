3e68bb2d1f59c865e422d092d2ec55f283bececf
nguyenminhdungtd98 <nguyenminhdungtd98@gmail.com>
2021-10-23 21:41:03 +0700

    Create url find all products

--- src/main/java/com/comaymanagement/cmd/controller/APIController.java
+++ src/main/java/com/comaymanagement/cmd/controller/APIController.java
@@ -1,7 +1,27 @@
 package com.comaymanagement.cmd.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.comaymanagement.cmd.entity.Product;
+import com.comaymanagement.cmd.service.ProductService;
+
+@RestController
+@RequestMapping("/")
 public class APIController {
 	
+	@Autowired
+	ProductService productService;
+	
+	// Create url find all products
+	@GetMapping("products")
+	public List<Product> FindAll(){
+		return productService.findAll();
+	}
 
 
 
--- src/main/java/com/comaymanagement/cmd/service/ProductService.java
+++ src/main/java/com/comaymanagement/cmd/service/ProductService.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.comaymanagement.cmd.entity.Product;
 import com.comaymanagement.cmd.repository.ProductRepositoty;
 
 @Service
 public class ProductService {
 
+	@Autowired
+	ProductRepositoty productRepositoty;
+	public List<Product> findAll(){
+		return productRepositoty.findAll();
+	}
 }
