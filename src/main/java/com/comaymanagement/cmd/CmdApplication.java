c1608a48d245ac357e756dbdb5a7dbb9922bc159
toann7700@gmail.com <toann7700@gmail.com>
2021-11-13 22:27:18 +0700

    .....

--- src/main/java/com/comaymanagement/cmd/CmdApplication.java
+++ src/main/java/com/comaymanagement/cmd/CmdApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CmdApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CmdApplication.class, args);
 		
 	}
